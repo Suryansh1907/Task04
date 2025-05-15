a17dddf0e79efdefbb5153648641075722dce749
Suryansh1907 <suryansh_tilak@epam.com>
2025-05-15 23:14:00 +0530

    Update versions.tf

--- task04/versions.tf
+++ task04/versions.tf
@@ -12,6 +12,9 @@ terraform {
 provider "azurerm" {
     
   features {}
-   
+  subscription_id = "759891c5-debe-4df2-af63-fd87bed98ad4"
+  tenant_id       = "f6540ec0-0c79-4f73-a028-07c98c1ae9d8"
+  client_id       = "09199365-fbc1-4c3d-abec-a0f5b1740826"
+  client_secret   = "qlp8Q~RUmkK2tfbI-.HAIYDxw1C0OeF5zrCW6bx7"
 
 }
